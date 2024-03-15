d99d6c2876c8019aa03c7d9c0475146f7c37a684
Paulandreaa <murcipaula54@gmail.com>
2024-03-14 19:56:25 -0500

    hola

--- ProyectoFinal/src/ProyectoFinal/Array.java
+++ ProyectoFinal/src/ProyectoFinal/Array.java
@@ -1,10 +1,45 @@
 package ProyectoFinal;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Array {
 
-    private ArrayList<String> lista = new ArrayList<>();
-    
-    
+    int cont = 1, op, numeral = 1, n;
+    String estudiante;
+    Scanner date = new Scanner(System.in);
+    ArrayList<String> lista = new ArrayList<>();
+
+    public static void agregarEstudiantes(int cont, String estudiante, Scanner date, ArrayList<String> lista, int op) {
+        while (op != 1) {
+            System.out.println("Ingrese estudiante numero:" + (cont));
+            estudiante = date.next();
+            lista.add(estudiante);
+            cont++;
+            System.out.println("¿Desea agregar otro estudiante?");
+            System.out.println("1. No");
+            System.out.println("2. Si");
+            op = date.nextInt();
+        }
+
+    }
+
+    public static int imprimirLista(ArrayList<String> lista, int numeral) {
+
+        if (!lista.isEmpty()) {
+            System.out.println("Lista Estudiantes");
+            for (int i = 0; i < lista.size(); i++) {
+                System.out.println(numeral + ") " + lista.get(i));
+                numeral++;
+            }
+        } else {
+            System.out.println("Lista vacia");
+        }
+        return numeral;
+    }
+
+    public static void agregarEstudiante() {
+        String nuevoEstudiante;
+
+    }
 }
--- ProyectoFinal/src/ProyectoFinal/ProyectoFinal.java
+++ ProyectoFinal/src/ProyectoFinal/ProyectoFinal.java
@@ -2,10 +2,14 @@ package ProyectoFinal;
 
 import java.io.*;
 import static ProyectoFinal.Arreglos.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ProyectoFinal {
     
     public static void main(String[] args) {
-        
+        Array p = new Array();
+        p.agregarEstudiantes(p.cont, p.estudiante, p.date, p.lista,p.op);
+        p.imprimirLista(p.lista, p.numeral);
     }
     
 }
